a474ba37ced0dd0988ddef3aa6a1540a388829dc
luc <luc@load.me>
2019-07-24 20:30:41 +0300

    fix

--- discriminant.php
+++ discriminant.php
@@ -16,24 +16,25 @@ function discriminant($a,$b,$c){
 
     if (0<$d) {
         $x1=($b*(-1)+sqrt($d))/($a*2);
-        $x2=($b*(-1)-sqrt($d))/($a*2);
+        $x2=($b*(-1)-sqrt($d))/($a*2).'<br>';
         echo 'x1='.$x1.'<br>';
         echo 'x2='.$x2;
+
     }
     else
     {
         if(0==$d) {
-            $x=($b*(-1))/($a*2);
+            $x=($b*(-1))/($a*2).'<br>';
          echo 'x='.$x;
         }
         else
         {
             if(0>$d)
-                echo 'нет решений';
+                echo 'нет решений'.'<br>';
         }
 
     }
-
+return $d;
 }
 
 echo discriminant(1,-2,-3).'<br>';
