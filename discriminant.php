718c4e2d2fbf0bf1f919715469c36819ccfc2818
luc <luc@load.me>
2019-07-29 20:54:24 +0300

    fix dz2

--- discriminant.php
+++ discriminant.php
@@ -8,34 +8,15 @@
 
 
 
-
-
-function discriminant($a,$b,$c){
-
-    $d=$b*$b-4*$a*$c;
-
-    if (0<$d) {
-        $x1=($b*(-1)+sqrt($d))/($a*2);
-        $x2=($b*(-1)-sqrt($d))/($a*2).'<br>';
-        echo 'x1='.$x1.'<br>';
-        echo 'x2='.$x2;
-
-    }
-    else
-    {
-        if(0==$d) {
-            $x=($b*(-1))/($a*2).'<br>';
-         echo 'x='.$x;
-        }
-        else
-        {
-            if(0>$d)
-                echo 'нет решений'.'<br>';
-        }
-
-    }
-return $d;
-}
+function discriminant($a, $b, $c){
+    return $b**2 - 4*$a*$c;
+};
+// d > 0
+assert(16 == discriminant(1,-2,-3));
+// d == 0
+assert(0 == discriminant(-2,0,0));
+// d < 0
+assert(-8 == discriminant(-1,-2,-3));
 
 echo discriminant(1,-2,-3).'<br>';
 echo discriminant(-2,0,0).'<br>';
